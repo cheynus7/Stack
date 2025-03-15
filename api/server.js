dd2ddc65cd444bd389e72dd65f2a407b265a526f
Jade Jadecheyne Cheyne <Jadecheyne@Jades-MacBook-Pro.local>
2025-03-16 10:07:45 +1100

    Add initial HTML structure for Stacking Block Game

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '../../')));
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
