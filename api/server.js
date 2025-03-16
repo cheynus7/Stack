53f1d711ff48ced69f137e9b74c224558ade0a9f
Jade Jadecheyne Cheyne <Jadecheyne@Jades-MacBook-Pro.local>
2025-03-16 11:22:15 +1100

    Ensure all files are correctly referenced and update vercel.json

--- api/server.js
+++ api/server.js
@@ -7,7 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the root directory
-app.use(express.static(path.join(__dirname, '../../')));
+app.use(express.static(path.join(__dirname, '../')));
+
+// Serve index.html on the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
